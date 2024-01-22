1124529dcd989c749826b91406032d8ce6dcfba7
Arshadansa <officialarshad16@gmail.com>
2024-01-22 18:04:37 +0530

    First-commit-with-navbar

---
+++ src/Components/Home.jsx
@@ -0,0 +1,116 @@
+import React, { useState, useEffect } from 'react';
+import logo from "../Images/logo.svg"
+import "../Css/home.css"
+import Main from '../Components/Main';
+
+const Home = () => {
+    // State variables to manage scroll behavior
+    const [prevScrollpos, setPrevScrollpos] = useState(window.pageYOffset);
+    const [top, setTop] = useState(0);
+    const handleScroll = () => {
+        const currentScrollPos = window.pageYOffset;
+        const isNavbarVisible = top === 0;
+
+        if (currentScrollPos === 0 && !isNavbarVisible) {
+            // When the user is at the top of the page and the navbar is not visible, show the navbar
+            setTop(0);
+
+        } else if (currentScrollPos > 0 && isNavbarVisible) {
+            // When scrolling down and the navbar is visible, hide the navbar
+            setTop(-130);
+
+
+        }
+    };
+
+    const handleMouseEnter = () => {
+        setTop(0);
+    };
+
+    const handleMouseLeave = () => {
+        setTop(-100);
+    };
+
+    useEffect(() => {
+        // Add scroll event listener when the component mounts
+        window.addEventListener('scroll', handleScroll);
+
+        // Clean up by removing the event listener when the component unmounts
+        return () => {
+            window.removeEventListener('scroll', handleScroll);
+        };
+    }, [top]);
+
+    const navbarStyle = {
+        backgroundColor: '#FAF9F6',
+        boxShadow: '0px 0px 20px gray',
+        position: 'fixed',
+        top: `${top}px`,
+        width: '100%',
+        height: '108px',
+        display: 'block',
+        transition: 'top 1s ease-in-out',
+    };
+
+
+
+
+    const linkStyle = {
+        float: 'left',
+        display: 'block',
+        color: '#f2f2f2',
+        textAlign: 'center',
+        padding: '15px',
+        textDecoration: 'none',
+        fontSize: '17px',
+    };
+    return (
+        <div>
+            <div id="navbar" style={navbarStyle} >
+                <nav class="   ">
+                    <div class="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
+                        <a href="#" class="flex items-center space-x-3 rtl:space-x-reverse">
+                            <img src={logo} class="" alt="Flowbite Logo" />
+                        </a>
+                        <div className='flex gap-5'>
+                            <div class="flex md:order-2  space-x-3 md:space-x-0 rtl:space-x-reverse">
+                                <button type="button" class=" btn-hover color-5   ">Order Now</button>
+                                <button data-collapse-toggle="navbar-cta" type="button" class="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden  focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-controls="navbar-cta" aria-expanded="false">
+                                    <svg class="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 17 14">
+                                        <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 1h15M1 7h15M1 13h15" />
+                                    </svg>
+                                </button>
+                            </div>
+                            <div class="items-center justify-between  hidden w-full md:flex md:w-auto md:order-1" id="navbar-cta">
+                                <ul class="flex flex-col font-medium p-4 md:p-0 mt-4 rounded-lg md:space-x-14 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0  ">
+                                    <li>
+                                        <a href="#" class="block py-2 px-3 md:p-0 text-[20px]  bg-[#FAF9F6] " aria-current="page">Home</a>
+                                    </li>
+                                    <li>
+                                        <a href="#" class="block py-2 px-3 md:p-0 text-[20px]  rounded bg-[#FAF9F6] ">Product</a>
+                                    </li>
+                                    <li>
+                                        <a href="#" class="block py-2 px-3 md:p-0 text-[20px]   rounded bg-[#FAF9F6] ">Sustainability</a>
+                                    </li>
+                                    <li>
+                                        <a href="#" class="in-block py-2 px-3 md:p-0 ml-5  text-[18px]  rounded bg-gradient-to-r from-[#022D24]  to-[#146C60]  text-transparent bg-clip-text  ">Login</a>
+                                    </li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                </nav>
+            </div>
+            <div
+                style={{
+                    padding: '0px 0px 2500px',
+                    fontSize: '30px',
+                    marginTop: '100px',
+                }}
+            >
+                <Main />
+            </div>
+        </div>
+    );
+};
+export default Home;
---
+++ src/Components/Main.jsx
@@ -0,0 +1,48 @@
+import React from 'react'
+import MologLogo from "../Images/molog logo 1.svg"
+import reactangle from "../Images/Group 2240.png"
+
+function Main() {
+
+    const boxShadowStyle = {
+        boxShadow: '5px 10px 14px 3px gray',
+    };
+
+
+    return (
+        <>
+            <div className='flex max-w-screen-xl pt-52 mx-auto items-center justify-between'>
+                <div className='flex flex-col gap-10'>
+                    <div className='leading-none'>
+                        <p className='text-[86px]  text-[#146C60]'>The professional way</p>
+                        <p className='text-[86px]   text-[#022D24]'>for networking</p>
+                        <span className=' text-[16px] text-[#717171]'>For those aiming to leave a fantastic initial impact.</span>
+                    </div>
+                    <div>
+                        <button className='btn-hover color-5 '>
+                            Get Your Card
+                        </button>
+                    </div>
+                </div>
+                <div className=''>
+                    <div className='w-[424px] shadow-[rgba(0,_0,_0,_0.44)_6px_5px_12px] flex flex-col justify-between h-[230px] p-10 bg-[#F5F2F0]   rounded-xl '>
+                        <div className='leading-3 '>
+                            <div><span className='text-[19px]'>FULL NAME</span></div>
+                            <div><span className='text-[11px]'>Employee’s Designation</span></div>
+                            <div><span className='text-[11px]'>+91 7047017862</span></div>
+                        </div>
+                        <div className='flex  w-full  items-end justify-end '>
+                            <img src={MologLogo} class=" h-[91px] mt-10 " alt="Molog Logo" />
+                        </div>
+                    </div>
+
+                </div>
+            </div >
+            <section className='my-32'>
+                <img src={reactangle} className='w-full' alt="" />
+            </section>
+        </>
+    )
+}
+
+export default Main
