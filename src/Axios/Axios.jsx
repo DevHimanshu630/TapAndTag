392ad4ff3255a7c3e613c11a75a1c0a6fd84414e
Shobhit-Gupta-Real <guptashobhit2024@gmail.com>
2024-03-30 12:52:25 +0530

    sign-in responsive

--- src/Axios/Axios.jsx
+++ src/Axios/Axios.jsx
@@ -3,8 +3,9 @@ import axios from "axios";
 const instance = axios.create({
   // baseURL: "http://13.200.74.2/",
   //  baseURL: "https://tapandtag.onrender.com/", //The API {Cloud Funtion}
-   baseURL: "https://qdp72jc1-8080.inc1.devtunnels.ms/",
+  //  baseURL: "https://qdp72jc1-8080.inc1.devtunnels.ms/",
   // baseURL: "https://j3hg2gqz-8080.inc1.devtunnels.ms/"
+  baseURL: "http://localhost:8080"
   
 });
 
