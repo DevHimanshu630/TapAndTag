000a97c68e460b49c738b1c88430504286235877
Himanshu singh <123050257+DevHimanshu630@users.noreply.github.com>
2024-04-02 11:43:22 +0530

    update

--- src/App.js
+++ src/App.js
@@ -37,7 +37,7 @@ function App() {
             <Route path="/vcard/:pageId" element={<VcardTemplate />} />
             <Route path='/qrform/:formId' element={<QrForm />} />
             <Route path='/qrform' element={<QrForm />} />
-            <Route path='/success' element={<Success />} />
+            {/* <Route path='/success' element={<Success />} /> */}
             <Route path='/filemanager' element={<FileManager />} />
             <Route path='/accounts/password/reset/' element={<Forgot />} />
             <Route path='/accounts/password/forgot' element={<Verification />} />
