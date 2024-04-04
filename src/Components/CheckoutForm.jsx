0febe37b72252a8f962db0fdbc7a657c360ea472
Shobhit-Gupta-Real <guptashobhit2024@gmail.com>
2024-04-04 18:03:40 +0530

    dashboard cart cartform

--- src/Components/CheckoutForm.jsx
+++ src/Components/CheckoutForm.jsx
@@ -1,37 +1,10 @@
 import React, { useState } from 'react'
-import cart from "../Images/Cart.svg"
 import Checkbox from '@mui/material/Checkbox';
-import { Unstable_NumberInput as BaseNumberInput, numberInputClasses } from '@mui/base/Unstable_NumberInput';
-import { styled } from '@mui/system';
 import axios from '../Axios/Axios';
-import carting from "../Images/Cart.svg"
 import { toast, ToastContainer } from "react-toastify";
+import CardCart from './CardCart';
+import AddShoppingCartIcon from '@mui/icons-material/AddShoppingCart';
 
-
-const label = { inputProps: { 'aria-label': 'Checkbox demo' } };
-
-const NumberInput = React.forwardRef(function CustomNumberInput(props, ref) {
-  return (
-    <BaseNumberInput
-      slots={{
-        root: StyledInputRoot,
-        input: StyledInputElement,
-        incrementButton: StyledButton,
-        decrementButton: StyledButton,
-      }}
-      slotProps={{
-        incrementButton: {
-          children: '▴',
-        },
-        decrementButton: {
-          children: '▾',
-        },
-      }}
-      {...props}
-      ref={ref}
-    />
-  );
-});
 
 function CheckoutForm({handleClose, selectedform, setOpen, setCartCall}) {
   const [cart, setCart] = useState({
@@ -39,36 +12,7 @@ function CheckoutForm({handleClose, selectedform, setOpen, setCartCall}) {
     woodCard : 0,
     metalCard: 0
   })
-  const [plastic, setPlastic] = useState(false)
-  const [wood, setWood] = useState(false)
-  const [metal, setMetal] = useState(false)
  
-  const handlePlastic = () => {
-    setPlastic(!plastic);
-    if (!plastic) {
-      setCart({ ...cart, plasticCard: 1});
-    } else {
-      setCart({...cart, plasticCard: 0});
-    }
-  };
-
-  const handleWood = () =>{
-    setWood(!wood);
-    if(!wood){
-        setCart({...cart, woodCard: 1});
-    }else {
-      setCart({...cart, woodCard: 0});
-    }
-  };
-
-  const handleMetal = () =>{
-    setMetal(!metal);
-    if(!metal){
-        setCart({...cart, metalCard: 1});
-    }else {
-      setCart({...cart, metalCard: 0});
-    }
-  };
   const token = localStorage.getItem("token");
   const handleSubmit = async () => {
     console.log(selectedform);
@@ -100,15 +44,8 @@ function CheckoutForm({handleClose, selectedform, setOpen, setCartCall}) {
   };
   
   console.log(cart)
-  const sectionStyle = {
-    backgroundImage: `url(${carting})`,
-    backgroundSize: 'cover', // Adjust the background size if needed
-    with: '240px',
-    height: '140px', // Set a specific height
-};
-
   return (
-    <div className='cart-container flex bg-green-200 gap-12 p-6 h-[90vh] w-[80vw]'>
+    <div className='cart-container flex gap-12 p-6 h-[90vh] w-[80vw]' style={{ backgroundColor: 'rgba(0, 0, 0, 0.5)', backdropFilter: 'blur(10px)', opacity: '0.9' }}>
     <div className='flex flex-col justify-between flex-1'>
     <button
                   type="button"
@@ -134,185 +71,21 @@ function CheckoutForm({handleClose, selectedform, setOpen, setCartCall}) {
     </button>
     <div className="card-img flex gap-12 justify-center">
         <span className='flex flex-col justify-center'>
-        <div style={sectionStyle} class="flex items-center font-[inter] font-bold justify-center text-white text-[20px] w-[240px]   ">
-            <p>Classic</p>
-        </div>
-        <Checkbox {...label} onClick={handlePlastic} checked={plastic}/>
-        {plastic? (<NumberInput
-        aria-label="Demo number input"
-        placeholder="Type a number…"
-        min={1}
-        value={cart.plasticCard}
-        onChange={(event, val)=> setCart({...cart, plasticCard: val})}
-        />):''}
+        <CardCart show={'Classic'} val={cart.plasticCard} data={'plasticCard'} cart={cart} setCart={setCart}/>
         </span>
         <span className='flex flex-col justify-center'>
-        <div style={sectionStyle} class="flex items-center  font-[inter] font-bold justify-center text-white text-[20px] w-[240px]   ">
-            <p>Wood</p>
-        </div>
-        <Checkbox {...label} onClick={handleWood} checked={wood}/>
-        {wood?(<NumberInput
-        aria-label="Demo number input"
-        placeholder="Type a number…"
-        min={1}
-        value={cart.woodCard}
-        onChange={(event, val)=> setCart({...cart, woodCard: val})}
-        />):''}
+        <CardCart show={'Wood'} val={cart.woodCard} data={'woodCard'} cart={cart} setCart={setCart}/>
         </span>
         <span className='flex flex-col justify-center'>
-        <div style={sectionStyle} class="flex items-center  font-[inter] font-bold justify-center text-white text-[20px] w-[240px]   ">
-            <p>Metal</p>
-        </div>
-        <Checkbox {...label} onClick={handleMetal} checked={metal}/>
-        {metal?(<NumberInput
-        aria-label="Demo number input"
-        placeholder="Type a number…"
-        min={1}
-        value={cart.metalCard}
-        onChange={(event, val)=> setCart({...cart, metalCard: val})}
-        />):''}
+        <CardCart show={'Metal'} val={cart.metalCard} data={'metalCard'} cart={cart} setCart={setCart}/>
         </span>
     </div>
-    <button onClick={handleSubmit} className='bg-green-500 hover:bg-green-600 text-white py-2 px-4 rounded-full w-[20%] self-end'>Save</button>
+    <button onClick={handleSubmit} className={cart.plasticCard || cart.metalCard || cart.woodCard ? 'bg-green-500 hover:bg-green-600 text-white py-2 px-2 rounded-full w-[20%] mx-4 self-end' : 'bg-slate-500 text-white py-2 px-4 mx-4 rounded-full w-[20%] self-end disabled'} 
+    disabled={!(cart.metalCard || cart.plasticCard || cart.woodCard )}>Add To Cart 
+    <AddShoppingCartIcon/></button>
     </div>
     </div>
   )
 }
 
 export default CheckoutForm
-const blue = {
-  100: '#DAECFF',
-  200: '#80BFFF',
-  400: '#3399FF',
-  500: '#007FFF',
-  600: '#0072E5',
-};
-
-const grey = {
-  50: '#F3F6F9',
-  100: '#E5EAF2',
-  200: '#DAE2ED',
-  300: '#C7D0DD',
-  400: '#B0B8C4',
-  500: '#9DA8B7',
-  600: '#6B7A90',
-  700: '#434D5B',
-  800: '#303740',
-  900: '#1C2025',
-};
-
-const StyledInputRoot = styled('div')(
-  ({ theme }) => `
-  font-family: 'IBM Plex Sans', sans-serif;
-  font-weight: 400;
-  border-radius: 8px;
-  color: ${theme.palette.mode === 'dark' ? grey[300] : grey[900]};
-  background: ${theme.palette.mode === 'dark' ? grey[900] : '#fff'};
-  border: 1px solid ${theme.palette.mode === 'dark' ? grey[700] : grey[200]};
-  box-shadow: 0px 2px 2px ${theme.palette.mode === 'dark' ? grey[900] : grey[50]};
-  display: grid;
-  grid-template-columns: 1fr 19px;
-  grid-template-rows: 1fr 1fr;
-  overflow: hidden;
-  column-gap: 8px;
-  padding: 4px;
-
-  &.${numberInputClasses.focused} {
-    border-color: ${blue[400]};
-    box-shadow: 0 0 0 3px ${theme.palette.mode === 'dark' ? blue[600] : blue[200]};
-  }
-
-  &:hover {
-    border-color: ${blue[400]};
-  }
-
-  // firefox
-  &:focus-visible {
-    outline: 0;
-  }
-`,
-);
-
-const StyledInputElement = styled('input')(
-  ({ theme }) => `
-  font-size: 0.875rem;
-  font-family: inherit;
-  font-weight: 400;
-  line-height: 1.5;
-  grid-column: 1/2;
-  grid-row: 1/3;
-  color: ${theme.palette.mode === 'dark' ? grey[300] : grey[900]};
-  background: inherit;
-  border: none;
-  border-radius: inherit;
-  padding: 8px 12px;
-  outline: 0;
-`,
-);
-
-const StyledButton = styled('button')(
-  ({ theme }) => `
-  display: flex;
-  flex-flow: row nowrap;
-  justify-content: center;
-  align-items: center;
-  appearance: none;
-  padding: 0;
-  width: 19px;
-  height: 19px;
-  font-family: system-ui, sans-serif;
-  font-size: 0.875rem;
-  line-height: 1;
-  box-sizing: border-box;
-  background: ${theme.palette.mode === 'dark' ? grey[900] : '#fff'};
-  border: 0;
-  color: ${theme.palette.mode === 'dark' ? grey[300] : grey[900]};
-  transition-property: all;
-  transition-timing-function: cubic-bezier(0.4, 0, 0.2, 1);
-  transition-duration: 120ms;
-
-  &:hover {
-    background: ${theme.palette.mode === 'dark' ? grey[800] : grey[50]};
-    border-color: ${theme.palette.mode === 'dark' ? grey[600] : grey[300]};
-    cursor: pointer;
-  }
-
-  &.${numberInputClasses.incrementButton} {
-    grid-column: 2/3;
-    grid-row: 1/2;
-    border-top-left-radius: 4px;
-    border-top-right-radius: 4px;
-    border: 1px solid;
-    border-bottom: 0;
-    &:hover {
-      cursor: pointer;
-      background: ${blue[400]};
-      color: ${grey[50]};
-    }
-
-  border-color: ${theme.palette.mode === 'dark' ? grey[800] : grey[200]};
-  background: ${theme.palette.mode === 'dark' ? grey[900] : grey[50]};
-  color: ${theme.palette.mode === 'dark' ? grey[200] : grey[900]};
-  }
-
-  &.${numberInputClasses.decrementButton} {
-    grid-column: 2/3;
-    grid-row: 2/3;
-    border-bottom-left-radius: 4px;
-    border-bottom-right-radius: 4px;
-    border: 1px solid;
-    &:hover {
-      cursor: pointer;
-      background: ${blue[400]};
-      color: ${grey[50]};
-    }
-
-  border-color: ${theme.palette.mode === 'dark' ? grey[800] : grey[200]};
-  background: ${theme.palette.mode === 'dark' ? grey[900] : grey[50]};
-  color: ${theme.palette.mode === 'dark' ? grey[200] : grey[900]};
-  }
-  & .arrow {
-    transform: translateY(-1px);
-  }
-`,
-);
--- src/VRcard/Dashboard.jsx
+++ src/VRcard/Dashboard.jsx
@@ -13,6 +13,7 @@ import { toast, ToastContainer } from "react-toastify";
 import Box from "@mui/material/Box";
 import Modal from "@mui/material/Modal";
 import CheckoutForm from "../Components/CheckoutForm";
+import DeleteForeverIcon from '@mui/icons-material/DeleteForever';
 const { format } = require('date-fns');
 
 function Dashboard() {
@@ -295,19 +296,21 @@ function Dashboard() {
                   </span>
                 </td>
                 <td className="px-6 py-3 flex hover:underline hover:cursor-pointer hover:text-blue-500 items-center gap-1 font-sans">
-                  <MdDeleteOutline
+                  <div className="">
+                  <DeleteForeverIcon
                     onClick={() => {
                       handleDelete(item.formDataID);
                     }}
                     size={20}
                   />
-                  <span
+                  </div>
+                  {/* <span
                     onClick={() => {
                       handleDelete(item.formDataID);
                     }}
                   >
                     delete
-                  </span>
+                  </span> */}
                 </td>
               </tr>
             ))}
