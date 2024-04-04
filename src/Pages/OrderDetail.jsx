e6317869bfa3497ae72bb3c7a2b7e8467aa7dd5b
Shobhit-Gupta-Real <guptashobhit2024@gmail.com>
2024-04-04 11:32:38 +0530

    refId fetch

--- src/Pages/OrderDetail.jsx
+++ src/Pages/OrderDetail.jsx
@@ -1,12 +1,11 @@
-import { useParams, useSearchParams } from 'react-router-dom'
+import { useParams } from 'react-router-dom'
 import axios from '../Axios/Axios'
 import React, { useEffect, useState } from 'react'
 
+
 function OrderDetail() {
   const {id} = useParams()
-  const searchQuery = useSearchParams();
-  const referenceId = searchQuery.get("referenceId");
-  console.log(referenceId)
+  const [refId, setRefId] = useState('')
   const token = localStorage.getItem("token")
   const [details, setDetails] = useState('')
   useEffect(()=>{
@@ -19,13 +18,20 @@ function OrderDetail() {
         console.log(res.data.order)
         setDetails(res.data.order)
     }
+    const gettingId = async() =>{
+      const Query = window.location.search
+      const urlQuery = new URLSearchParams(Query)
+      setRefId(urlQuery.get('referenceId'));
+    }
     getting()
+    gettingId()
   },[])
   
   return (
     <div className='flex flex-col gap-12'>
       Payment Status : {details.paymentStatus}<br/>
-      Here we can fetch the details 
+      Here we can fetch the details <br />
+      {refId}
 
     <button class="bg-red-500 hover:bg-red-600 text-white font-bold py-2 px-4 rounded">
     Red Button
