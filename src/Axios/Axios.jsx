62555d873e872d85faabc0be81a094497d29615a
Shobhit-Gupta-Real <guptashobhit2024@gmail.com>
2024-04-08 16:59:05 +0530

    axios

--- src/Axios/Axios.jsx
+++ src/Axios/Axios.jsx
@@ -2,10 +2,10 @@ import axios from "axios";
 
 const instance = axios.create({
   // baseURL: "http://13.200.74.2/",
-  //  baseURL: "https://tapandtag.onrender.com/", //The API {Cloud Funtion}
+   baseURL: "https://tapandtag.onrender.com/", //The API {Cloud Funtion}
   // baseURL: "https://qdp72jc1-8080.inc1.devtunnels.ms/",
   // baseURL: "https://j3hg2gqz-8080.inc1.devtunnels.ms/"
-  baseURL: 'http://localhost:8080'
+  // baseURL: 'http://localhost:8080'
   // baseURL: 'ec2-3-110-32-74.ap-south-1.compute.amazonaws.com'
 });
 
